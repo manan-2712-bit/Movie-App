313fd024feb1a72821ad6132a57845c83efe8f14
Manan <mananparashar723@gmail.com>
2026-06-14 10:49:34 +0530

    first commit

--- frontend project/src/services/api.js
+++ frontend project/src/services/api.js
@@ -1,5 +1,5 @@
 const API_KEY="f92224049f0775d86e3a733c3a69b0ab";
-const BASE_URL="http://api.tmdb.org/3"
+const BASE_URL="https://api.tmdb.org/3"
 
 export const getPopularMovies = async ()=>{
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
